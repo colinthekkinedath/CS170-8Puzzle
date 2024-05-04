434e7afc56f998fbf15ee263fe1635d4fe946b34
Keshav-Mannem <keshavmannem023@gmail.com>
2024-05-04 12:37:13 -0700

    added node class to main

--- puzzle.py
+++ puzzle.py
@@ -195,5 +195,17 @@ def expand(node, visited):
 
     return node
 
+class node:
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+        self.cost = 0
+        self.depth = 0
+        self.parent = None
+        self.child1 = None
+        self.child2 = None
+        self.child3 = None
+        self.child4 = None
+        self.expanded = False
+        
 if __name__ == "__main__":
     main()
