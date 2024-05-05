617952275db2c8da93022a15536cd319de1a9d73
colinthekkinedath <colinthekkinedath@gmail.com>
2024-05-04 19:50:46 -0700

    refactored input

--- puzzle.py
+++ puzzle.py
@@ -16,17 +16,15 @@ def main():
     elif inputnum == 2:
         print('Enter your puzzle, use a zero to represent the blank')
 
-        row1 = input("Enter the first row, use space or tabs between numbers\n")
-        row2 = input("Enter the second row, use space or tabs between numbers\n")
-        row3 = input("Enter the third row, use space or tabs between numbers\n")
+        rows = []
+        for i in range(3):
+            row_input = input(f"Enter row {i+1}, use space or tabs between numbers\n")
+            row_values = row_input.split()
+            rows.append(row_values)
 
         print('\n')
 
-        row1 = row1.split(' ')
-        row2 = row2.split(' ')
-        row3 = row3.split(' ')
-
-        puzzle = row1, row2, row3
+        puzzle = tuple(rows)
 
 
     inputalgo = input('Enter your choice of algorithm \n1. Uniform Cost Search \n2. A* with the Misplaced Tile heuristic. \n3. A* with the Euclidean distance heuristic.\n')
